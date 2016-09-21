fe72b3faf5bc886dbd0b1e68723a7a7c8cbe6016
Robert Maguire <LazyWizard@users.noreply.github.com>
2016-09-21 14:14:31 -0500

    Added documentation

--- downloader.py
+++ downloader.py
@@ -9,6 +9,7 @@ from imgurpython.helpers.error import ImgurClientError
 
 
 def __create_client() -> ImgurClient:
+    #  Set up ImgurClient with our OAuth2 credentials, obtained here: https://api.imgur.com/oauth2/addclient
     config = configparser.ConfigParser()
     config.read('credentials.ini')
     client_id = config.get('client', 'id')
@@ -23,6 +24,7 @@ def __get_image_links(album_id: str) -> List[str]:
 
     result = []
     for img in images:
+        #  Large GIFs will have an MP4 version (.gifv) as well, we should prefer these as they are much smaller
         link = img.mp4 if hasattr(img, 'mp4') else img.link
         result.append(link.replace('http:', 'https:'))
     return result
@@ -33,11 +35,13 @@ def __save_images(folder_name: str, images: List[str]):
     os.makedirs(path, exist_ok=True)
     total_attempted = total_complete = total_bytes = 0
     for link in images:
+        #  Don't download the image if we already have a copy
         local_path = path + '/' + os.path.basename(link)
         if os.path.isfile(local_path):
             print('Already downloaded: ' + link)
             continue
 
+        #  Download image and save to disk, save total bytes downloaded for later reporting
         print('Downloading ' + link + ' ... ', end='')
         total_attempted += 1
         try:
@@ -57,6 +61,12 @@ def __save_images(folder_name: str, images: List[str]):
 
 
 def download_album(album_id: str):
+    """
+    Downloads all images in a public album to its own directory, found under images/<album id>/.
+
+    Args:
+        album_id: the ID of the album, the part after https://imgur.com/a/
+    """
     print("Attempting to download album '{}'".format(album_id))
     try:
         __save_images(album_id, __get_image_links(album_id))
@@ -65,6 +75,15 @@ def download_album(album_id: str):
 
 
 def download_account(account_name: str):
+    """
+    Downloads all public albums of an account. Each album is downloaded to its own directory as if download_album
+    had been called on each one separately (which is indeed what happens under the hood).
+
+
+    Args:
+        account_name: the ID of the account, found by clicking on a username and taking the part of the URL
+        before imgur.com
+    """
     print("Attempting to download account '{}'".format(account_name))
     try:
         album_ids = __client.get_account_album_ids(account_name)
@@ -78,8 +97,10 @@ def download_account(account_name: str):
 
 if __name__ == '__main__':
     args = sys.argv[1:]
+    #  If command line arguments are passed in, assume it's a list of album IDs
     if args:
         for arg in args:
             download_album(arg)
+    #  Otherwise prompt the user for input
     else:
         download_account(input('Please pass in an account name or album id: '))
